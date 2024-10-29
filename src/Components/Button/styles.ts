3c9cfd1233e1114413ce7acbf3ecf7ab6435bc2b
Leonardo <l.guerarti@gmail.com>
2024-10-29 20:57:31 -0300

    Componentização
    Criação de componentes reutilizáveis para a aplicação

---
+++ src/Components/Button/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const ComponentButton = styled.TouchableOpacity`
+  margin-top: 20px;
+  background-color: #ea8720;
+  padding: 20px 60px;
+  border-radius: 6px;
+  margin-bottom: 20px;
+  align-items: center;
+`;
